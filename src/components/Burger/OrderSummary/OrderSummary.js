f2f9ce5d217ca46c1197842a1b631f9611b76450
alina.galas <sem1920dark>
2019-11-14 16:47:00 +0200

    fix

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,37 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = props => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            return (
-                <li key={igKey}>
-                    <span 
-                        style={{
-                            textTransform: 'capitalize'
-                        }}
-                    >{igKey}</span> : {props.ingredients[igKey]}
-                </li>
-            )
-        });
+class OrderSummary extends Component {
+    componentWillUpdate() {
+        console.log('[OrderSummary] WillUpdate')
+    }
 
-    return(
-        <Aux>
-            <h3>Your order</h3>
-            <p>A delicious burger with the following ingredients: </p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p>
-                <strong>Total Price: {props.price.toFixed(2)}</strong>
-            </p>
-            <p>Continue to Checkout?</p>
-            <Button btnType='Danger' clicked={props.purchaseCancelled}>Cancel</Button>
-            <Button btnType='Success' clicked={props.purchaseContinued}>Continue</Button>
-        </Aux>
-    )
+    render() {
+        const { ingredients, price, purchaseCancelled, purchaseContinued } = this.props;
+
+        const ingredientSummary = Object.keys(ingredients)
+            .map(igKey => {
+                return (
+                    <li key={igKey}>
+                        <span 
+                            style={{
+                                textTransform: 'capitalize'
+                            }}
+                        >{igKey}</span> : {ingredients[igKey]}
+                    </li>
+                )
+            })
+
+        return(
+            <Aux>
+                <h3>Your order</h3>
+                <p>A delicious burger with the following ingredients: </p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p>
+                    <strong>Total Price: {price.toFixed(2)}</strong>
+                </p>
+                <p>Continue to Checkout?</p>
+                <Button btnType='Danger' clicked={purchaseCancelled}>Cancel</Button>
+                <Button btnType='Success' clicked={purchaseContinued}>Continue</Button>
+            </Aux>
+        )
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './Modal.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = props => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div 
-            className='Modal'
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}
-        >{props.children}</div>
-    </Aux>
-);
+class Modal extends Component {
+    render() {
+        const { show, modalClosed, children} = this.props;
 
-export default modal;
+        return (
+            <Aux>
+                <Backdrop show={show} clicked={modalClosed}/>
+                <div 
+                    className='Modal'
+                    style={{
+                        transform: show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: show ? '1' : '0'
+                    }}
+                >{children}</div>
+            </Aux>
+        )
+    }
+};
+
+export default Modal;
