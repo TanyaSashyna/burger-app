fabd05b48054633c553bcc92228c68377ce10f43
alina.galas <sem1920dark>
2019-11-14 15:48:35 +0200

    added sideBar

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,15 +3,23 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = props => {
+    let attachedClasses = ['SideDrawer', 'Close'];
+    if (props.open) attachedClasses = ['SideDrawer', 'Open'];
+
     return(
-        <div className='SideDrawer'>
-            <Logo/>
-            <nav>
-                <NavigationItems/>
-            </nav>
-        </div>
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <Logo/>
+                <nav>
+                    <NavigationItems/>
+                </nav>
+            </div>
+        </Aux>
     );
 };
 
