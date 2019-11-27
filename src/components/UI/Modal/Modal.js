83fc66e62417ac7699a8c45bc6a5291d435a2d65
alina.galas <sem1920dark>
2019-11-27 18:20:34 +0200

    changed structure

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 
 import './Modal.css';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
-    render() {
+    shouldComponentUpdate (nextProps, nextState) {
+        return nextProps.show !==this.props.show
+    }
+
+    componentWillUpdate () {
+        console.log('[Modal] WillUpdate')
+    }
+
+    render () {
         const { show, modalClosed, children} = this.props;
 
         return (
